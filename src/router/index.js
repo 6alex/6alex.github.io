6c880fb186123350104de1309c68ac20f7dac925
wencun <810815091@qq.com>
2019-04-30 12:24:14 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import PagesView from '../views/PagesView'
-import HomeView from '../views/HomeView'
-import MovieView from '../views/MovieView'
-import BookView from '../views/BookView'
-import StatusView from '../views/StatusView'
-import GroupView from '../views/GroupView'
-import SubjectView from '../views/SubjectView'
-import DetailView from '../views/DetailView'
-import SearchView from '../views/SearchView'
-import LoginView from '../views/LoginView'
-import RegisterView from '../views/RegisterView'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -19,77 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/pages/'
-    },
-    {
-      path: '/pages',
-      component: PagesView,
-      children: [
-        {
-          path: '',
-          redirect: '/pages/home'
-        },
-        {
-          path: 'home',
-          name: 'HomeView',
-          component: HomeView
-        },
-        {
-          path: 'movie',
-          name: 'MovieView',
-          component: MovieView
-        },
-        {
-          path: 'book',
-          name: 'BookView',
-          component: BookView
-        },
-        {
-          path: 'status',
-          name: 'StatusView',
-          component: StatusView
-        },
-        {
-          path: 'group',
-          name: 'GroupView',
-          component: GroupView
-        },
-        {
-          path: 'detail/:id',
-          name: 'DetailView',
-          component: DetailView
-        }
-      ]
-    },
-    {
-      path: '/pages/:classify/subject/:id',
-      name: 'SubjectView',
-      components: {
-        default: PagesView,
-        subject: SubjectView
-      }
-    },
-    {
-      path: '/search',
-      name: 'SearchView',
-      components: {
-        default: PagesView,
-        search: SearchView
-      }
-    },
-    {
-      path: '/login',
-      name: 'LoginView',
-      component: LoginView
-    },
-    {
-      path: '/register',
-      name: 'RegisterView',
-      component: RegisterView
-    },
-    {
-      path: '*',
-      redirect: '/pages/'
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
